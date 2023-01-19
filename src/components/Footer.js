acf1f55907d595189284658c0fbefc887cf47d0c
Maiconjrs <maiconjrs@hotmail.com>
2023-01-19 19:45:00 -0300

    change footer

--- src/components/Footer.js
+++ src/components/Footer.js
@@ -2,7 +2,7 @@ import styles from './Footer.module.css';
 
 const Footer = () => {
   return <footer className={styles.footer}>
-        <h3>Escreva oque vc tem interesse</h3>
+        <h3>Cadastre-se agora e comece a postar sobre assuntos oque você tem interesse</h3>
         <p>Blog Dev &copy; 2023</p>
   </footer>
 }
